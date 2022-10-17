1f02dd205a18449325d10e5eb7653e6cfe2d0033
priscila espinoza <pr.espinoza@duocuc.cl>
2022-10-17 01:30:00 -0300

    archivos static, entorno virtual

--- hello.py
+++ hello.py
@@ -1,9 +1,10 @@
 #primero debemos ejecutar el pipenv install flask
 #luego pipenv shell
-#luego py (nombre del archivor).py py hello.py
+#luego py (nombre del archivor).py hello.py
 
 
-from flask import Flask  # Aca estoy importanto flask para crear una aplicación web
+from flask import Flask, render_template  # Aca estoy importanto flask para crear una aplicación web y ingresando una plantilla 
+
 
 app = Flask(__name__) # aqui estoy creando una nueva instancia de la calse Flask llamada app
 
@@ -15,6 +16,11 @@ def hola_mundo ():
 def hola(nombre):
     return f'<h1>Hola {nombre}, cómo estás?</h1>' # la f nos permite que con las llaves poner el nombre de la variable de forma más sencilla
     #tambien puedo utilizar la etiquetas directamente aqui 
+
+@app.route('/hello/<name>/<int:j>')
+def hello(name, j):
+# En lugar de devolver una cadena, devolvemos el resultado de un render_templates
+     return render_template('hola.html', nombre=name, cantidad=j )
 
 
 
